2d1cca8fd6580eb91a9b25457991572491a8cab7
Lina Segers <45152162+linasegers@users.noreply.github.com>
2025-02-07 11:44:06 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
-install.packages("IRkernel")
+install.packages("remotes")  # Ensures we can install from GitHub
+install.packages("IRkernel", repos="http://cran.us.r-project.org")
 IRkernel::installspec(user = TRUE)
 install.packages(c("ggplot2", "dplyr", "tidyverse"))  # Add other required packages
